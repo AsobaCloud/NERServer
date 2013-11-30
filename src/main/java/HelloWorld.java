114b1a351f7a70c557ba79b5d750ee07137a5f4e
Ankush Shah <shah@Param.(none)>
2013-11-30 18:29:21 +0100

    everything working on localhost

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,15 +4,33 @@ import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.*;
+import edu.stanford.nlp.io.IOUtils;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations;
+
+import java.util.List;
+import java.io.IOException;
+
 public class HelloWorld extends HttpServlet {
 	
-	
+	String serializedClassifier = "english.muc.7class.distsim.crf.ser.gz";
+    AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
 	
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        resp.getWriter().print("Hello from Java!\n");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	String text = req.getParameter("text");
+    	String output =  classifier.classifyWithInlineXML(text);
+        resp.getWriter().print(output);
     }
+    
+    private void loadClassifier()
+    {
+    	
+        
+    }
+    
     public static void main(String[] args) throws Exception{
         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
